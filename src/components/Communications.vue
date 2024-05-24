da6dcea20c90dfa8d41a928e210129a38b535c13
Andrii Yurchuk <andrexonerhg@gmail.com>
2024-05-25 00:13:50 +0300

    FIXED length undefined

--- src/components/Communications.vue
+++ src/components/Communications.vue
@@ -16,8 +16,7 @@ const profileName = ref("");
 const allowToWriteComment = ref(null);
 
 const currentUserComment = computed(() => commentsStore.myComment);
-const comments = computed(() => commentsStore.myComment);
-const commentWasSentByThisUser = ref(false);
+const comments = computed(() => commentsStore.comments);
 
 const disableCommentButton = ref(false);
 const commentError = ref("");
